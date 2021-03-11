57834e045db26c13eb86fd148ac0fbb39f148ad7
Royal <thaparoyal17@gmail.com>
2021-03-11 10:50:41 +0545

    working with classes

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 'use strict';
-
+// Defining Variables
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnClosedModal = document.querySelector('.closed-modal')
-const btnsOpenModal = document.querySelectorAll('.show-modal')
+const btnClosedModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-for (let i = 0; i < btnsOpenModal.length ; i++){
-    console.log(btnsOpenModal[i].textContent);
-}
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnClosedModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
