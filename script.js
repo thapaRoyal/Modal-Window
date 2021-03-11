48f265beea5f7e4c7cdeebca9aaf82961b420cad
Royal <thaparoyal17@gmail.com>
2021-03-11 10:29:35 +0545

    Defining Variables

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClosedModal = document.querySelector('.closed-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+console.log(btnsOpenModal);
+
+
+for (let i = 0; i < btnsOpenModal.length ; i++){
+    console.log(btnsOpenModal[i].textContent);
+}
